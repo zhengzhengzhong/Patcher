ece8b0b03a9543bf1d923f57504f6d792f5e128f
zhengzhengzhong <zhengzhengzhong@outlook.com>
2017-04-03 21:44:43 +0800

    first commit
    first commit

---
+++ run/bin/run.bat
@@ -0,0 +1,13 @@
+:: base dir
+set BASE=%~dp0\..
+:: lib
+set LIB=%BASE%\lib\
+:: log4j
+set LOG4J=%BASE%\etc\log4j.properties
+
+set JVM_OPT=-Dlog4j=%LOG4J%
+set JVM_OPT=%JVM_OPT% -Droot.dir=%BASE%
+set JVM_OPT=%JVM_OPT% -Djava.ext.dirs=%LIB%
+set MAIN_CLASS=cn.letterme.tools.patcher.GUILauncher
+
+java %JVM_OPT% %MAIN_CLASS%
